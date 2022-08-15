e0fd2488238ba5f9645e3f275d25eba5333426a4
Freextt <olehsam@gmail.com>
2022-08-16 01:10:30 +0300

    HW_2_add

--- lesson_2/HW_2_add.py
+++ lesson_2/HW_2_add.py
@@ -34,14 +34,47 @@ func1()
 func2()
 func2()
 func1()
+
+
 # вивести послідовність Фібоначі, кількість вказана в знінній,
 #   наприклад: x = 10 -> 1 1 2 3 5 8 13 21 34 55
 #   (число з послідовності - це сума попередніх двох чисел)
 #
-# def fib(num):
+def fib(num):
+    a = 0
+    b = 1
+    if num < 0:
+        print('Input correct value')
+    elif num == 0:
+        return 0
+    elif num == 1:
+        return b
+    else:
+        for i in range(1, num):
+            c = a + b
+            a = b
+            b = c
+        return b
 
 
-# fib(10)
+print('*' * 10)
+print(fib(10))
+
+
 # порахувати кількість парних і непарних цифр числа,
 #   наприклад: х = 225688 -> п = 5, н = 1;
 #          х = 33294 -> п = 2, н = 3
+def even_or_odd(num):
+    even = 0
+    odd = 0
+    res = list(map(int, str(num)))
+    for i in res:
+        if i % 2 == 0:
+            even += 1
+        elif i % 2 == 1:
+            odd += 1
+    print('Even:' + str(even), 'Odd:' + str(odd))
+    return even, odd
+
+
+print(even_or_odd(225688))
